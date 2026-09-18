6135d397ffc35af16d1e92ae55f5d66c65bec341
Brandon <btoms.20@gmail.com>
2026-09-18 15:10:50 -0700

    support swift 6.0 strict member import visibility

--- Sources/LibP2PCore/Event/PeerDiscoveryEvent.swift
+++ Sources/LibP2PCore/Event/PeerDiscoveryEvent.swift
@@ -12,7 +12,7 @@
 //
 //===----------------------------------------------------------------------===//
 
-import Multiaddr
+public import Multiaddr
 
 public enum PeerDiscoveryEvent: Sendable {
     //case ready
